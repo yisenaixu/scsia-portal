316b278b504bbaad66ecbfaef5bd10961199b59c
yisenaixu <1852570928@qq.com>
2023-11-08 17:16:10 +0800

    fix: 顶层await修改

--- src/main.js
+++ src/main.js
@@ -11,26 +11,6 @@ import { getRoutes, setRoutes } from './utils/router';
 
 const app = createApp(App) 
 
-if(router.options.routes.length === router.getRoutes().length) {
-    let routes = [];
-    console.debug(111);
-    // if(sessionStorage.getItem('routes')) {
-    //   routes = JSON.parse(sessionStorage.getItem('routes'));
-    //   for(let i of routes) {
-    //       router.addRoute(i);
-    //       router.options.routes.push(i);
-    //   }
-    //   console.log(router.options.routes);
-    //   console.debug(routes);
-    //   store.commit('update',routes);
-    //   console.log(222);
-    // } else {
-      routes = await getRoutes();
-      setRoutes(routes);
-      console.log(333);
-//   }
-  }
-
 app.use(router);
 app.use(store);
 app.mount('#app')
