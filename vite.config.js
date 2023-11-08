65c044a411f6cf7f7667420f8bd1b26712d17062
Aliothmoon <aliothmoon@outlook.com>
2023-11-08 21:12:45 +0800

    feat: add top-level await plugin

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import {resolve} from 'node:path'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
+import topLevelAwait from "vite-plugin-top-level-await";
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/portal': {
-        target : 'http://120.76.192.186:8080/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/protal/,'protal')
-      },
+    server: {
+        proxy: {
+            '/portal': {
+                target: 'http://120.76.192.186:8080/',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/protal/, 'protal')
+            },
+        }
+    },
+    plugins: [
+        vue(),
+        createSvgIconsPlugin({
+            iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+            symbolId: "icon-[name]"
+        }),
+        topLevelAwait({
+            // The export name of top-level await promise for each chunk module
+            promiseExportName: "__tla",
+            // The function to generate import names of top-level await promise in each chunk module
+            promiseImportName: i => `__tla_${i}`
+        })
+
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  },
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      iconDirs: [resolve(process.cwd(),'src/assets/icons')],
-      symbolId: "icon-[name]"
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
