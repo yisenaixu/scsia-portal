4e4c2965636c6287e38fa932611023a9f70a80eb
Aliothmoon <aliothmoon@outlook.com>
2023-11-09 01:10:17 +0800

    fix: 修复路由加载错误 -> `server responded with a MIME type of "text/html".`

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
 import topLevelAwait from "vite-plugin-top-level-await";
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/",
     server: {
         proxy: {
             '/portal': {
